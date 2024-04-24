4697913824f90f3407b2d79bb7725cbebdf59b98
huangwl <410488219@qq.com>
2024-04-24 09:04:11 +0800

    refactor: add interface for sftp service

---
+++ src/main/kotlin/ez/sftp/ISftpService.kt
@@ -0,0 +1,46 @@
+package ez.sftp
+
+import com.jcraft.jsch.ChannelSftp
+import com.jcraft.jsch.ChannelSftp.LsEntry
+import java.nio.file.Path
+
+interface ISftpService {
+  /**
+   * 切换工作目录（除非[relativePath]为空，否则会返回一个新的[SftpService]实例）
+   * @param relativePath 相对路径。默认为空字符串，表示保持现有目录不变
+   */
+  fun cd(relativePath: String = ""): ISftpService
+
+  /**
+   * 根据数据ID生成子目录（相对路径），再切换到此目录。
+   * - 例如，[id]为123000789，层数为4，每层位数为3，则生成的子目录为`000/123/000/789`（相对于当前工作目录）
+   */
+  fun cd(id: Long): ISftpService
+
+  /**
+   * 执行SFTP操作
+   * @param action 待执行的SFTP操作
+   * @return 执行结果
+   */
+  fun <T> exec(action: (ChannelSftp) -> T): T?
+
+  /**
+   * 上传文件
+   * @param localPath 本地文件路径
+   * @param remoteFileName 远程文件名（可选）。如果为空，则使用本地文件名（临时文件会使用[getRealName]处理）
+   */
+  fun upload(localPath: Path, remoteFileName: String? = null)
+
+  /**
+   * 下载文件
+   * @param remoteFileName 远程文件名
+   * @param localPath 本地文件路径（可选）。如果为空，则下载到临时文件夹
+   */
+  fun download(remoteFileName: String, localPath: Path? = null): Path
+
+  /**
+   * 列出目录下的文件
+   * @param path 目录路径（可选）。默认为空字符串，表示当前目录
+   */
+  fun ls(path: String = ".") = exec { it.ls(path) }!!.map { it as LsEntry }
+}
--- src/main/kotlin/ez/sftp/SftpService.kt
+++ src/main/kotlin/ez/sftp/SftpService.kt
@@ -1,7 +1,6 @@
 package ez.sftp
 
 import com.jcraft.jsch.ChannelSftp
-import com.jcraft.jsch.ChannelSftp.LsEntry
 import io.github.hligaty.haibaracp.core.SftpSession
 import io.github.hligaty.haibaracp.core.SftpTemplate
 import org.slf4j.LoggerFactory
@@ -15,7 +14,7 @@ class SftpService private constructor(
    */
   private val remoteDir: String,
   private val sftpTemplate: SftpTemplate<SftpSession>
-) {
+) : ISftpService {
   init {
     ensureRemoteDirectoryExists(remoteDir)
   }
@@ -23,38 +22,30 @@ class SftpService private constructor(
   constructor(config: SftpAutoConfig, sftpTemplate: SftpTemplate<SftpSession>) :
     this(config, config.remoteDir.removeSuffix("/"), sftpTemplate)
 
-  /**
-   * 切换工作目录（除非[relativePath]为空，否则会返回一个新的[SftpService]实例）
-   * @param relativePath 相对路径。默认为空字符串，表示保持现有目录不变
-   */
-  fun cd(relativePath: String = ""): SftpService {
+  override fun cd(relativePath: String): SftpService {
     if (relativePath.isEmpty()) return this
     val remoteDir = "$remoteDir/$relativePath".removeSuffix("/")
     return SftpService(config, remoteDir, sftpTemplate)
   }
 
-  /**
-   * 根据数据ID生成子目录（相对路径），再切换到此目录。
-   * - 例如，[id]为123000789，层数为4，每层位数为3，则生成的子目录为`000/123/000/789`（相对于当前工作目录）
-   */
-  fun cd(id: Long): SftpService {
+  override fun cd(id: Long): SftpService {
     val relativePath = idToPath(id, config.idLevels, config.digitsPerLevel)
     return cd(relativePath)
   }
 
-  fun <T> exec(action: (ChannelSftp) -> T) = sftpTemplate.execute {
+  override fun <T> exec(action: (ChannelSftp) -> T) = sftpTemplate.execute {
     it.cd(remoteDir)
     action(it)
   }
 
-  fun upload(localPath: Path, remoteFileName: String? = null) {
+  override fun upload(localPath: Path, remoteFileName: String?) {
     val finalRemoteFileName = remoteFileName ?: localPath.getRealName()
     exec {
       it.put(localPath.toAbsolutePath().toString(), finalRemoteFileName)
     }
   }
 
-  fun download(remoteFileName: String, localPath: Path? = null): Path {
+  override fun download(remoteFileName: String, localPath: Path?): Path {
     val finalLocalPath = localPath ?: Files.createTempFile("${remoteFileName}.", null)!!
     try {
       exec {
@@ -66,8 +57,6 @@ class SftpService private constructor(
     }
     return finalLocalPath
   }
-
-  fun ls(path: String = ".") = exec { it.ls(path) }!!.map { it as LsEntry }
 
   private fun ensureRemoteDirectoryExists(remoteDirPath: String) {
     val dirs = remoteDirPath.split('/').filter { it.isNotEmpty() } // 分割路径并去除空字符串
