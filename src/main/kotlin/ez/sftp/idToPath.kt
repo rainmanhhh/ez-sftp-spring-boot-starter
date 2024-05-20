e926a775880167da4fc9a7b58e6c8136ec802e57
huangwl <410488219@qq.com>
2024-05-20 17:06:30 +0800

    fix: idLevels为1时，id转换为path直接返回它的字符串形式，不需要在前面补0

--- src/main/kotlin/ez/sftp/idToPath.kt
+++ src/main/kotlin/ez/sftp/idToPath.kt
@@ -3,9 +3,11 @@ package ez.sftp
 /**
  * Convert an ID to a path string based on the specified levels and digitsPerLevel.
  * - eg: idToPath(23456789, 4, 3) returns "000/023/000/789"
+ * - special case: if levels is 1, return the ID as a string
  */
 fun idToPath(id: Long, levels: Int, digitsPerLevel: Int): String {
   val idStr = id.toString() // eg. "23000789"
+  if (levels == 1) return idStr
   val totalLengthWithoutSlash = levels * digitsPerLevel // 4 * 3 = 12 (length of "000023000789")
   val totalLength = totalLengthWithoutSlash + levels // 12 + 4 = 16 (length of "000/023/000/789/")
   val padLength = totalLengthWithoutSlash - idStr.length // number of padding zeros at the start, here is 4
